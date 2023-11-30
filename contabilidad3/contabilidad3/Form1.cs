c53df38ba81bb522aa5e5f87cfb30a81e2338ffc
Avato <avenuefourdot@gmail.com>
2023-11-29 23:29:10 -0800

    aded app version 2

---
+++ contabilidad3/contabilidad3/Form1.cs
@@ -0,0 +1,178 @@
+namespace contabilidad3
+{
+    public partial class Form1 : Form
+    {
+        Datos datos = new Datos();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Empresa_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Almacen_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Almacen_Carga_Click(object sender, EventArgs e)
+        {
+            datos.AlmacenCargos.Add(double.Parse(Almacen.Text));
+            Almacen.Text = "";
+
+        }
+
+        private void Almacen_Abono_Click(object sender, EventArgs e)
+        {
+            datos.AlmacenAbonos.Add(double.Parse(Almacen.Text));
+            Almacen.Text = "";
+        }
+
+        private void Ventas_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Ventas_Carga_Click(object sender, EventArgs e)
+        {
+            datos.VentasCargos.Add(double.Parse(Ventas.Text));
+            Ventas.Text = "";
+        }
+        private void Ventas_Abono_Click(object sender, EventArgs e)
+        {
+            datos.VentasAbonos.Add(double.Parse(Ventas.Text));
+            Ventas.Text = "";
+        }
+
+        private void CostosVentas_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CostosVentas_Carga_Click(object sender, EventArgs e)
+        {
+            datos.CostosVentasCargos.Add(double.Parse(CostosVentas.Text));
+            CostosVentas.Text = "";
+        }
+
+        private void CostosVentas_Abono_Click(object sender, EventArgs e)
+        {
+            datos.CostosVentasAbonos.Add(double.Parse(CostosVentas.Text));
+            CostosVentas.Text = "";
+        }
+
+        private void Bancos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bancos_Carga_Click(object sender, EventArgs e)
+        {
+            datos.BancosCargos.Add(double.Parse(Bancos.Text));
+            Bancos.Text = "";
+        }
+
+        private void Bancos_Abono_Click(object sender, EventArgs e)
+        {
+            datos.BancosCargos.Add(double.Parse(Bancos.Text));
+            Bancos.Text = "";
+        }
+
+        private void Capital_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Capital_Carga_Click(object sender, EventArgs e)
+        {
+            datos.CapitalCargos.Add(double.Parse(Capital.Text));
+            Capital.Text = "";
+        }
+
+        private void Capital_Abono_Click(object sender, EventArgs e)
+        {
+            datos.CapitalAbonos.Add(double.Parse(Capital.Text));
+            Capital.Text = "";
+        }
+
+        private void Proveedores_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Proveedores_Carga_Click(object sender, EventArgs e)
+        {
+            datos.ProveedoresCargos.Add(double.Parse(Proveedores.Text));
+            Proveedores.Text = "";
+        }
+
+        private void Proveedores_Abono_Click(object sender, EventArgs e)
+        {
+            datos.ProveedoresAbonos.Add(double.Parse(Proveedores.Text));
+            Proveedores.Text = "";
+        }
+
+        private void Caja_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Caja_Carga_Click(object sender, EventArgs e)
+        {
+            datos.CajaCargos.Add(double.Parse(Caja.Text));
+            Caja.Text = "";
+        }
+
+        private void Caja_Abono_Click(object sender, EventArgs e)
+        {
+            datos.CajaAbonos.Add(double.Parse(Caja.Text));
+            Caja.Text = "";
+        }
+
+        private void Clientes_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clientes_Carga_Click(object sender, EventArgs e)
+        {
+            datos.ClientesCargos.Add(double.Parse(Clientes.Text));
+            Clientes.Text = "";
+        }
+
+        private void Clientes_Abono_Click(object sender, EventArgs e)
+        {
+            datos.ClientesAbonos.Add(double.Parse(Clientes.Text));
+            Clientes.Text = "";
+        }
+
+        
+        private void Utilidades_Cargar_Click(object sender, EventArgs e)
+        {
+            datos.UtilidadesPerdidasCargos.Add(double.Parse(Utilidades.Text));
+            Utilidades.Text = "";
+        }
+
+        private void Utilidades_Abonar_Click(object sender, EventArgs e)
+        {
+            datos.UtilidadesPerdidasAbonos.Add(double.Parse(Utilidades.Text));
+            Utilidades.Text = "";
+        }
+        private void Importar_Click(object sender, EventArgs e)
+        {
+            datos.TituloEmpresa = Empresa.Text;
+            System.Diagnostics.Debug.WriteLine(datos.TituloEmpresa);
+            Procedimiento._datos = datos;
+            Procedimiento.Importar();
+        }
+
+    }
+}
---
+++ contabilidad3/contabilidad3/Procedimiento.cs
@@ -0,0 +1,216 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contabilidad3
+{
+    internal class Procedimiento
+    {
+        public static Datos _datos { get; set; }
+        public static void Importar()
+        {
+            FileInfo file = new FileInfo($"Inventario_{_datos.TituloEmpresa}.xlsx");
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Inventario"];
+                if (worksheet == null)
+                {
+                    worksheet = package.Workbook.Worksheets.Add("Inventario");
+                }
+                worksheet.Cells["A1:K1"].Merge = true;
+                worksheet.Cells["A1"].Value = _datos.TituloEmpresa;
+                worksheet.Cells[$"A1:K1"].Style.Border.Bottom.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                //Primeros 4
+                int maxRowCount = Math.Max(_datos.AlmacenCargos.Count, _datos.AlmacenAbonos.Count);
+                int fila = 2;
+                worksheet.Cells["A2:B2"].Merge = true;
+                worksheet.Cells["A2"].Value = "Almacen";
+                worksheet.Cells[$"A{3}:B{3}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < maxRowCount; i++)
+                {
+                    if (i < _datos.AlmacenCargos.Count)
+                    {
+                        worksheet.Cells[$"A{fila + 1}"].Value = _datos.AlmacenCargos[i];
+                        worksheet.Cells[$"A{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    }
+
+                    if (i < _datos.AlmacenAbonos.Count)
+                    {
+                        worksheet.Cells[$"B{fila + 1}"].Value = _datos.AlmacenAbonos[i];
+                    }
+                    fila++;
+                }
+                worksheet.Cells[$"A{fila + 1}:B{fila+1}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Double;
+                worksheet.Cells[$"A{fila+1}"].Formula = $"SUM(A3:A{fila})";
+                worksheet.Cells[$"A{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                worksheet.Cells[$"B{fila+1}"].Formula = $"SUM(B3:B{fila})";
+                worksheet.Cells[$"A{fila+2}"].Formula = $"(A{fila+1} - B{fila+1})";
+                worksheet.Cells[$"A{fila + 2}:B{fila + 2}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Double;
+
+                fila = 2;
+                worksheet.Cells["D2:E2"].Merge = true;
+                worksheet.Cells["D2"].Value = "Proveedores";
+                worksheet.Cells[$"D{3}:E{3}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < _datos.ProveedoresCargos.Count; i++)
+                {
+                    worksheet.Cells[$"D{fila + 1}"].Value = _datos.ProveedoresCargos[i];
+                    worksheet.Cells[$"D{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    fila++;
+                }
+                fila = 2;
+                for (int i = 0; i < _datos.ProveedoresAbonos.Count; i++)
+                {
+                    worksheet.Cells[$"E{fila + 1}"].Value = _datos.ProveedoresAbonos[i];
+                    fila++;
+                }
+                
+                fila = 2;
+                worksheet.Cells["G2:H2"].Merge = true;
+                worksheet.Cells["G2"].Value = "Caja";
+                worksheet.Cells[$"G{3}:H{3}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < _datos.CajaCargos.Count; i++)
+                {
+                    worksheet.Cells[$"G{fila + 1}"].Value = _datos.CajaCargos[i];
+                    worksheet.Cells[$"G{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    fila++;
+                }
+                fila = 2;
+                for (int i = 0; i < _datos.CajaAbonos.Count; i++)
+                {
+                    worksheet.Cells[$"H{fila + 1}"].Value = _datos.CajaAbonos[i];
+                    fila++;
+                }
+                
+                fila = 2;
+                worksheet.Cells["J2:K2"].Merge = true;
+                worksheet.Cells["J2"].Value = "Capital";
+                worksheet.Cells[$"J{3}:K{3}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < _datos.CapitalCargos.Count; i++)
+                {
+                    worksheet.Cells[$"J{fila + 1}"].Value = _datos.CapitalCargos[i];
+                    worksheet.Cells[$"J{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    fila++;
+                }
+                fila = 2;
+                for (int i = 0; i < _datos.CapitalAbonos.Count; i++)
+                {
+                    worksheet.Cells[$"K{fila + 1}"].Value = _datos.CapitalAbonos[i];
+                    fila++;
+                }
+
+                //Ultimos 5
+                fila = 12;
+                worksheet.Cells["A12:B12"].Merge = true;
+                worksheet.Cells["A12"].Value = "Bancos";
+                worksheet.Cells[$"A{13}:B{13}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < _datos.BancosCargos.Count; i++)
+                {
+                    worksheet.Cells[$"A{fila + 1}"].Value = _datos.BancosCargos[i];
+                    worksheet.Cells[$"A{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    fila++;
+                }
+                fila = 12;
+                for (int i = 0; i < _datos.BancosAbonos.Count; i++)
+                {
+                    worksheet.Cells[$"B{fila + 1}"].Value = _datos.BancosAbonos[i];
+                    fila++;
+                }
+
+                maxRowCount = Math.Max(_datos.VentasCargos.Count, _datos.VentasAbonos.Count);
+                fila = 12;
+                worksheet.Cells["D12:E12"].Merge = true;
+                worksheet.Cells["D12"].Value = "Ventas";
+                worksheet.Cells[$"D{13}:E{13}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < maxRowCount; i++)
+                {
+                    if (i < _datos.VentasCargos.Count)
+                    {
+                        worksheet.Cells[$"D{fila + 1}"].Value = _datos.VentasCargos[i];
+                        worksheet.Cells[$"D{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    }
+
+                    if (i < _datos.VentasAbonos.Count)
+                    {
+                        worksheet.Cells[$"E{fila + 1}"].Value = _datos.VentasAbonos[i];
+                    }
+                    fila++;
+                }
+                worksheet.Cells[$"D{fila + 1}"].Formula = $"SUM(D13:D{fila})";
+                worksheet.Cells[$"D{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                worksheet.Cells[$"E{fila + 1}"].Formula = $"SUM(E13:E{fila})";
+                worksheet.Cells[$"D{fila+1}:E{fila+1}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Double;
+
+
+                maxRowCount = Math.Max(_datos.CostosVentasCargos.Count, _datos.CostosVentasAbonos.Count);
+                fila = 12;
+                worksheet.Cells["G12:H12"].Merge = true;
+                worksheet.Cells["G12"].Value = "Costos de ventas";
+                worksheet.Cells[$"G{13}:H{13}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < maxRowCount; i++)
+                {
+                    if (i < _datos.CostosVentasCargos.Count)
+                    {
+                        worksheet.Cells[$"G{fila + 1}"].Value = _datos.CostosVentasCargos[i];
+                        worksheet.Cells[$"G{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    }
+
+                    if (i < _datos.CostosVentasAbonos.Count)
+                    {
+                        worksheet.Cells[$"H{fila + 1}"].Value = _datos.CostosVentasAbonos[i];
+                    }
+                    fila++;
+                }
+                worksheet.Cells[$"G{fila + 1}"].Formula = $"SUM(G13:G{fila})";
+                worksheet.Cells[$"G{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                worksheet.Cells[$"H{fila + 1}"].Formula = $"SUM(H13:H{fila})";
+                worksheet.Cells[$"G{fila + 1}:H{fila + 1}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Double;
+
+                fila = 12;
+                worksheet.Cells["J12:K12"].Merge = true;
+                worksheet.Cells["J12"].Value = "Clientes";
+                worksheet.Cells[$"J{13}:K{13}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < _datos.ClientesCargos.Count; i++)
+                {
+                    worksheet.Cells[$"J{fila + 1}"].Value = _datos.ClientesCargos[i];
+                    worksheet.Cells[$"J{fila + 1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    fila++;
+                }
+                fila = 12;
+                for (int i = 0; i < _datos.ClientesAbonos.Count; i++)
+                {
+                    worksheet.Cells[$"K{fila + 1}"].Value = _datos.ClientesAbonos[i];
+                    fila++;
+                }
+
+                fila = 22;
+                worksheet.Cells["A22:B22"].Merge = true;
+                worksheet.Cells["A22"].Value = "Pérdidas o Ganancias";
+                worksheet.Cells[$"A{23}:B{23}"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                for (int i = 0; i < _datos.UtilidadesPerdidasCargos.Count; i++)
+                {
+                    worksheet.Cells[$"A{fila+1}"].Value = _datos.UtilidadesPerdidasCargos[i];
+                    worksheet.Cells[$"A{fila+1}"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+                    fila++;
+                }
+                fila = 22;
+                for (int i = 0; i < _datos.UtilidadesPerdidasAbonos.Count; i++)
+                {
+                    worksheet.Cells[$"B{fila + 1}"].Value = _datos.UtilidadesPerdidasAbonos[i];
+                    fila++;
+                }
+
+                ExcelRangeBase dataRange = worksheet.Cells[$"A2:{worksheet.Dimension.End.Address}"];
+                dataRange.Style.Numberformat.Format = "$###,###,##0.00";
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+                worksheet.Cells.Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                worksheet.Cells.Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                package.Save();
+            }
+        }
+    }
+}
